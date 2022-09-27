05229906b68a0bb326724ee6b551f0873b02bb9e
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-09-27 22:01:04 +0200

    Versió retocada

--- CaracterRepetit/src/main/java/org/example/Main.java
+++ CaracterRepetit/src/main/java/org/example/Main.java
@@ -11,36 +11,52 @@ public class Main {
         System.out.println(caracterNoRepetit(s));
     }
 
-    public static String caracterRepetit(String s) {
-        Map<String, Integer> map = new HashMap<>();
+    public static char caracterRepetit(String s) {
+        //Mètode 1
 
+        //Inicialitzem
+        Set<Integer> intMap = new HashSet<>();
+        //Filtrem els caràcters repetits (el Set no pot tenir repeticions) i obtenim el primer que no s'ha pogut afegir
+        System.out.println((char) s.chars().filter(character -> !intMap.add(character)).findFirst().orElse(0));
+
+        //Mètode 2
+
+        //Inicialitzem
+        Set<Character> map = new HashSet<>();
+
+        //Recorrem el string
         for (int i = 0; i < s.length(); i++) {
-            String current = String.valueOf(s.charAt(i));
-            if (map.containsKey(current)) {
+            //Obtenim el caràcter actual
+            char current = s.charAt(i);
+            //Si el mapa ja conté el caràcter actual
+            if (map.contains(current)) {
+                //Retornem
                 return current;
             } else {
-                map.put(current, 1);
+                //Si no, l'afegim al mapa
+                map.add(current);
             }
         }
-        return "N/A";
+        //Retornem caràcter buit
+        return '\0';
     }
 
-    public static String caracterNoRepetit(String s) {
-        Map<String, Integer> map = new LinkedHashMap<>();
+    public static char caracterNoRepetit(String s) {
+        //Inicialitzem
+        Map<Character, Integer> map = new LinkedHashMap<>();
 
+        //Recorrem el string
         for (int i = 0; i < s.length(); i++) {
-            String current = String.valueOf(s.charAt(i));
-            Integer count = map.getOrDefault(current, 1);
-            if (map.containsKey(current)) {
-                map.put(current, count + 1);
-            } else {
-                map.put(current, count);
-            }
+            //Caràcter actual
+            char current = s.charAt(i);
+            //Comprovem si el caràcter ja existeix al set; si ho fa, incrementem el seu compte
+            map.put(current, map.containsKey(current) ? map.get(current) + 1 : 1);
         }
+        //Trobem el primer caràcter que apareix només una vegada al string i el retornem; si no, caràcter buit
         return map.entrySet().stream()
                 .filter(element -> element.getValue() == 1)
                 .findFirst()
                 .map(Map.Entry::getKey)
-                .orElse("N/A");
+                .orElse('\0');
     }
 }
